16a63cae4d2f43ff940c145a13d946ab81b816ff
Cousin Ze <alex@ckx-ily.cn>
2025-12-20 11:32:27 +0800

    删除一些花里胡哨的没用玩意

---
+++ README.md
@@ -0,0 +1,26 @@
+# LambdaPi
+
+Yet another (modern) Java implementation of the famous [LambdaPi.pdf](https://www.andres-loeh.de/LambdaPi/LambdaPi.pdf).
+
+## Progress
+
+- [ ] Java implementation
+  - [x] Tokenize
+  - [x] Syntax
+    - [ ] Pretty printing
+  - [ ] Parse
+  - [ ] STLC (λ<sub>→</sub>)
+    - [x] Representation
+      - [ ] Pretty printing
+    - [ ] Evaluator
+    - [ ] Type checker
+    - [ ] Quoting
+  - [ ] DTLC (λ<sub>Π</sub>)
+    - [ ] Representation
+    - [ ] Evaluator + Type checker
+- [ ] Python implementation
+  - This is a garbage programming language, but I think there might should be one.
+
+## Contribution
+
+You don't.
--- src/main/java/club/doki7/lambdapi/stlc/Term.java
+++ src/main/java/club/doki7/lambdapi/stlc/Term.java
@@ -1,16 +1,137 @@
 package club.doki7.lambdapi.stlc;
 
+import club.doki7.lambdapi.syntax.Node;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public sealed interface Term {
+    @NotNull Node node();
+
     sealed interface Inferable extends Term permits Ann, Bound, Free, App {}
     sealed interface Checkable extends Term permits Inf, Lam {}
 
-    record Ann(@NotNull Checkable term, @NotNull Type annotation) implements Inferable {}
-    record Bound(int index) implements Inferable {}
-    record Free(@NotNull Name name) implements Inferable {}
-    record App(@NotNull Inferable f, @NotNull Checkable arg) implements Inferable {}
+    record Ann(@NotNull Node node, @NotNull Checkable term, @NotNull Type annotation)
+            implements Inferable
+    {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Ann ann)) return false;
 
-    record Inf(@NotNull Inferable term) implements Checkable {}
-    record Lam(@NotNull Checkable body) implements Checkable {}
+            return term.equals(ann.term) && annotation.equals(ann.annotation);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(term, annotation);
+        }
+
+        @Override
+        public @NotNull String toString() {
+            throw new UnsupportedOperationException("Not supported yet");
+        }
+    }
+
+    record Bound(@NotNull Node node, int index) implements Inferable {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Bound bound)) return false;
+
+            return this.index == bound.index;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(Bound.class, index);
+        }
+
+        @Override
+        public @NotNull String toString() {
+            return Integer.toString(index);
+        }
+    }
+
+    record Free(@NotNull Node node, @NotNull Name name) implements Inferable {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Free free)) return false;
+
+            return this.name.equals(free.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(Free.class, name);
+        }
+
+        @Override
+        public @NotNull String toString() {
+            return name.toString();
+        }
+    }
+
+    record App(@NotNull Node node, @NotNull Inferable f, @NotNull Checkable arg)
+            implements Inferable
+    {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof App app)) return false;
+
+            return this.f.equals(app.f) && this.arg.equals(app.arg);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(f, arg);
+        }
+
+        @Override
+        public @NotNull String toString() {
+            throw new UnsupportedOperationException("Not supported yet");
+        }
+    }
+
+    record Inf(@NotNull Node node, @NotNull Inferable term) implements Checkable {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Inf inf)) return false;
+
+            return this.term.equals(inf.term);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(Inf.class, term);
+        }
+
+        @Override
+        public @NotNull String toString() {
+            return term.toString();
+        }
+    }
+
+    record Lam(@NotNull Node node, @NotNull Checkable body) implements Checkable {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Lam lam)) return false;
+
+            return this.body.equals(lam.body);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(Lam.class, body);
+        }
+
+        @Override
+        public @NotNull String toString() {
+            return "λ " + body;
+        }
+    }
 }
